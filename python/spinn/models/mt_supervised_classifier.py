Move to GPU + masking and attn fixes

python/spinn/models/mt_supervised_classifier.py
@@ -20,7 +20,7 @@ import spinn.util.evalb as evalb
 import spinn.util.logging_pb2 as pb
 from spinn.util.trainer import ModelTrainer
 import tempfile
-
+from time import gmtime, strftime
 # PyTorch
 import torch
 import torch.nn as nn
@@ -66,8 +66,9 @@ def evaluate(FLAGS, model, eval_set, log_entry,
         pyramid_temperature_multiplier = None
 
     model.eval()
-    ref_file_name=FLAGS.log_path+"/ref_file"
-    pred_file_name=FLAGS.log_path+"/pred_file"
+    curtime=strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    ref_file_name=FLAGS.log_path+"/ref_file"#+curtime
+    pred_file_name=FLAGS.log_path+"/pred_file"#+curtime
     reference_file=open(ref_file_name, "w")
     predict_file=open(pred_file_name, "w")
     full_ref=[]
@@ -85,6 +86,7 @@ def evaluate(FLAGS, model, eval_set, log_entry,
             validate_transitions=FLAGS.validate_transitions,
             pyramid_temperature_multiplier=pyramid_temperature_multiplier,
             example_lengths=eval_num_transitions_batch)
+        #import pdb;pdb.set_trace()
         can_sample = FLAGS.model_type in ["ChoiPyramid", "Maillard", "CatalanPyramid"] or (
             FLAGS.model_type == "SPINN" and FLAGS.use_internal_parser)
         if show_sample and can_sample:
@@ -100,31 +102,19 @@ def evaluate(FLAGS, model, eval_set, log_entry,
         done=[]
         for x in output:
             index=-1
-            for x_0 in x[0]:
+            for x_0 in x:
                 index+=1
                 val=int(x_0)
                 if val==1:
                     if index in done:
                         continue
                     done.append(index)
-                else:
+                elif index not in done:
                     predicted[index].append(val)
-        pred_out =[" ".join(map(str,k[:-1]))+" ." for k in predicted]
+        pred_out =[" ".join(map(str,k))+" ." for k in predicted]
         full_pred+=pred_out       
-        # Calculate class accuracy.
-        # target = torch.from_numpy(eval_y_batch).long()
-
-        # # get the index of the max log-probability
-        # pred = output.data.max(1, keepdim=False)[1].cpu()
 
         eval_accumulate(model, A, batch)
-        # criterion = nn.NLLLoss()
-        # trg_seq_len=trg.shape[0]
-        # mt_loss=0.0
-        # for i in range(trg_seq_len):
-        #     mt_loss+=criterion(output[i], trg[i,:].view(-1))
-        # batch_size=len(eval_y_batch)
-        # mt_loss/=batch_size
 
         # Optionally calculate transition loss/acc.
         model.transition_loss if hasattr(model, 'transition_loss') else None
@@ -137,6 +127,7 @@ def evaluate(FLAGS, model, eval_set, log_entry,
     reference_file.close()
     predict_file.write("\n".join(full_pred))
     predict_file.close()
+    print(full_pred)
     bleu_score=os.popen("perl spinn/util/multi-bleu.perl "+ ref_file_name+" < "+pred_file_name).read()
     print("BLEU"+bleu_score) 
     bleu_score=float(bleu_score)
@@ -217,7 +208,7 @@ def train_loop(
             pyramid_temperature_multiplier = None
 
         # Run model.
-        output, trg, attention = model(
+        output, trg, attention, mask = model(
             X_batch,
             transitions_batch,
             y_batch,
@@ -226,32 +217,43 @@ def train_loop(
             pyramid_temperature_multiplier=pyramid_temperature_multiplier,
             example_lengths=num_transitions_batch)
         criterion = nn.NLLLoss()
-        trg_seq_len=trg.shape[0]
-        mt_loss=0.0
-        for i in range(trg_seq_len):
-            mt_loss+=criterion(output[i], trg[i,:].view(-1))
         batch_size=len(y_batch)
-        mt_loss/=batch_size
+        trg_seq_len=trg.shape[0]
+        print(output[:,1].argmax(1))
+        print(trg[:,1])
+        print(output[:,0].argmax(1))
+        print(trg[:,0])
+        mt_loss=0.0
+        num_classes=output.shape[-1]
+        mask=to_gpu(mask)
+        # import pdb;pdb.set_trace()
+        # #mt_loss=criterion(output.t().contiguous().view(-1, num_classes), Variable(trg.view(-1), volatile=False))
+        for i in range(trg_seq_len):
+            #import pdb;pdb.set_trace()
+            mt_loss+=criterion(output[i,:].index_select(0, mask[i].nonzero().squeeze(1)), trg[i].index_select(0, mask[i].nonzero().squeeze(1)).view(-1))
         # Optionally calculate transition loss.
+        mt_loss=mt_loss/trg_seq_len
         transition_loss = model.transition_loss if hasattr(
             model, 'transition_loss') else None
         model.mt_loss=mt_loss
         # Accumulate Total Loss Variable
         total_loss = 0.0
         total_loss += mt_loss
-        if transition_loss is not None and model.optimize_transition_loss:
-            total_loss += transition_loss
+        # if transition_loss is not None and model.optimize_transition_loss:
+        #     total_loss += transition_loss
         aux_loss = auxiliary_loss(model)
         total_loss= total_loss + aux_loss
         # Backward pass.
+        aa = list(model.parameters())[-1].clone()
         total_loss.backward()
-
+        #[(x,y.grad) for x,y in model.named_parameters()]
         # Hard Gradient Clipping
         nn.utils.clip_grad_norm([param for name, param in model.named_parameters() if name not in ["embed.embed.weight"]], FLAGS.clipping_max_value)
 
         # Gradient descent step.
         trainer.optimizer_step()
-
+        bb = list(model.parameters())[-1].clone()
+        #import pdb;pdb.set_trace()
         end = time.time()
 
         total_time = end - start

python/spinn/spinn_core_model.py
@@ -462,11 +462,11 @@ class SPINN(nn.Module):
 
             batch = list(zip(transition_arr, self.bufs, self.stacks, self.tracker.states if hasattr(
                 self, 'tracker') and self.tracker.h is not None else itertools.repeat(None)))
-
+            reduced_idxs=[]
             for batch_idx, (transition, buf, stack,
                             tracking) in enumerate(batch):
                 if transition == T_SHIFT:  # shift
-                    attended[batch_idx].append(buf[-1][0])
+                    #attended[batch_idx].append(buf[-1][0])
                     self.t_shift(buf, stack, tracking, s_tops, s_trackings)
                     s_idxs.append(batch_idx)
                     s_stacks.append(stack)
@@ -478,6 +478,9 @@ class SPINN(nn.Module):
                         r_lefts,
                         r_rights,
                         r_trackings)
+                    reduced_idxs.append(batch_idx)
+                    attended[batch_idx].append(r_lefts[-1][0].unsqueeze(0))
+                    attended[batch_idx].append(r_rights[-1][0].unsqueeze(0))
                     #self.attended[batch_idx].append(buf[-1])
                     r_stacks.append(stack)
                     r_idxs.append(batch_idx)
@@ -489,7 +492,6 @@ class SPINN(nn.Module):
             self.shift_phase(s_tops, s_trackings, s_stacks)
             self.reduce_phase(r_lefts, r_rights, r_trackings, r_stacks)
             self.reduce_phase_hook(r_lefts, r_rights, r_trackings, r_stacks)
-            
             # Memory Phase
             # ============
 
@@ -548,7 +550,8 @@ class SPINN(nn.Module):
                 "two zeros and the sentence encoding."
             assert all(len(buf) == 1 for buf in self.bufs), \
                 "Stacks should be fully shifted and have 1 zero."
-        [attended[i].append(self.stacks[i][-1][0]) for i in range(batch_size)]
+        [attended[i].append(self.stacks[i][-1][0].unsqueeze(0)) for i in range(batch_size)]
+
         return [stack[-1]
                 for stack in self.stacks], transition_acc, transition_loss, attended
 
@@ -624,13 +627,14 @@ class BaseModel(nn.Module):
         else:
             sys.path.append(onmt_module)
             from onmt.decoders.decoder import InputFeedRNNDecoder, StdRNNDecoder, RNNDecoderBase
+            from onmt.encoders.rnn_encoder import RNNEncoder
             from onmt.modules import Embeddings
-            #import onmt
             self.output_embeddings=Embeddings(self.model_dim, len(target_vocabulary)+1, 0)
             self.decoder=StdRNNDecoder("LSTM", False, 1,self.model_dim, embeddings=self.output_embeddings)
             self.target_vocabulary=target_vocabulary
             self.generator=nn.Sequential(
-                nn.Linear(self.model_dim, len(self.target_vocabulary), nn.LogSoftmax())
+                nn.Linear(self.model_dim, len(self.target_vocabulary)+1),
+                nn.LogSoftmax()
             )
             #self.generator = nn.Sequential(nn.Linear(self.model_dim, len(self.target_vocabulary), nn.LogSoftmax())
         self.embedding_dropout_rate = 1. - embedding_keep_rate
@@ -683,8 +687,13 @@ class BaseModel(nn.Module):
         self.spinn.reset_state()
         h_list, transition_acc, transition_loss, attended = self.spinn(
             example, use_internal_parser=use_internal_parser, validate_transitions=validate_transitions)
+        maxlen_attended=max([len(x) for x in attended])
+        attended=[x+(maxlen_attended-len(x))*[torch.zeros(1, self.model_dim)] for x in attended]
+        wrapped_attended=[torch.cat((self.wrap(x)[0], torch.zeros(len(x), self.model_dim/2)),1 ) for x in attended]
+        #import pdb;pdb.set_trace()
+        wrapped_attended=torch.cat([x.unsqueeze(1) for x in wrapped_attended],1)
         h = self.wrap(h_list)
-        return h, transition_acc, transition_loss, attended
+        return h, transition_acc, transition_loss, wrapped_attended
 
     def forward_hook(self, embeds, batch_size, seq_length):
         pass
@@ -730,7 +739,6 @@ class BaseModel(nn.Module):
         h, transition_acc, transition_loss , attended= self.run_spinn(
             example, use_internal_parser, validate_transitions)        
         self.spinn_outp = h
-
         self.transition_acc = transition_acc
         self.transition_loss = transition_loss
         self.attention_h=attended
@@ -740,8 +748,11 @@ class BaseModel(nn.Module):
             target_maxlen=max([len(x) for x in y_batch])
             maxlen= example.tokens.size()[1]#max([len(x) for x in attended])
             tmp_trg=[]
+            t_mask=[]
             for x in y_batch:
-                arr=x+[1]*(target_maxlen-len(x))
+                arr=np.array(list(x)+[1]*(target_maxlen-len(x)))
+                t_mask.append([1]*(len(x)+1)+[0]*(target_maxlen-len(x)))
+                #arr=x+[1]*(target_maxlen-len(x))
                 tmp=[]
                 for y in arr:
                     la=y
@@ -749,41 +760,68 @@ class BaseModel(nn.Module):
                 tmp_trg.append(tmp)
             trg=[]
             batch_size=example.tokens.size()[0]
+            t_tmask_trg=[]
             for i in range(target_maxlen):
                 tmp=[]
+                tmp_mask=[]
                 for j in range(batch_size):
                     tmp.append(tmp_trg[j][i])
+                    tmp_mask.append(t_mask[j][i])
                 trg.append(tmp)
+                t_tmask_trg.append(tmp_mask)
+            #import pdb;pdb.set_trace()
             actual_dim=self.spinn_outp[0].shape[-1]
             enc_output=self.spinn_outp[0].view(1,batch_size, actual_dim)
-            padded_enc_output=torch.zeros((1, batch_size, self.model_dim))
+            padded_enc_output=to_gpu(torch.zeros((1, batch_size, self.model_dim)))
             padded_enc_output[:,:,:actual_dim]=enc_output
-            trg=torch.tensor(trg).view((target_maxlen, batch_size,nfeat))
-            #src=torch.view(embeds, (example.tokens.size()[1], example.tokens.size()[0], self.model_dim))
-            src=embeds.view((example.tokens.size()[1], batch_size, self.model_dim))
-            att=[x+(maxlen-len(x))*[torch.zeros(self.model_dim)] for x in attended]
-            att=[torch.cat(x).view((maxlen,self.model_dim)) for x in att]
-            att=torch.cat(att,1).view((maxlen, batch_size,self.model_dim))
-            enc_state=self.decoder.init_decoder_state(src, att, (padded_enc_output,padded_enc_output))
+            trg=torch.tensor(np.array(trg)).view((target_maxlen, batch_size,nfeat)).long()
+            trg=to_gpu(Variable(trg, volatile=not self.training))
+            src=torch.cat([torch.cat(x[::-1]).unsqueeze(0) for x in example.bufs]).t()           
+            #return output
+            # att=[x+(maxlen-len(x))*[torch.zeros(self.model_dim)] for x in attended]
+            # att=[torch.cat(x).view((maxlen,self.model_dim)) for x in att]
+            # att=torch.cat(att,1).view((maxlen, batch_size,self.model_dim))
+            #(padded_enc_output,padded_enc_output)
+            enc_state=self.decoder.init_decoder_state(src, attended, (padded_enc_output, padded_enc_output))
+            target_forced=False
             if self.training:
-                output_decoder=self.decoder(trg, att,enc_state)
-                output=self.generator(output_decoder[0])
+                if target_forced:
+                    decoder=self.decoder(trg, attended, enc_state)
+                    output=self.generator(decoder[0])
+                else:
+                    unk_token=torch.zeros((1, batch_size, 1)).long()
+                    inp=unk_token
+                    dec_state=enc_state
+                    xx=(padded_enc_output, padded_enc_output)
+                    output=[]
+                    for i in range(target_maxlen+1):
+                        if i==0:
+                            inp=unk_token
+                        else:
+                            inp=trg[i-1].unsqueeze(0)
+                        dec_out, dec_state, attn=self.decoder(inp, attended, dec_state, step=i)
+                        out=self.generator(dec_out.squeeze(0))
+                        argmaxed=torch.max(out,1)[1]
+                        inp=argmaxed.unsqueeze(1).unsqueeze(0)
+                        output.append(out.unsqueeze(0))
+                    output=torch.cat(output)
             else:#now just predict:
                 unk_token=torch.zeros((1, batch_size, 1)).long()
                 inp=unk_token
                 maxpossible=100
                 dec_state=enc_state
                 predicted=[]
+                xx=(padded_enc_output, padded_enc_output)
                 #TODO: replace with k-beam search
-                for i in range(maxpossible):
-                    #run one step
-                    dec_out, dec_state, attn = self.decoder(inp, att, dec_state, step=i)
-                    out=self.generator(dec_out)
-                    argmaxed=torch.max(out,2)[1]
-                    inp=argmaxed.unsqueeze(2)
-                    predicted.append(list(argmaxed))
+                #inp= trg[0].unsqueeze(0)
+                for i in range(100):
+                    dec_out, dec_state, attn = self.decoder(inp, attended, dec_state, step=i)
+                    out=self.generator(dec_out.squeeze(0))
+                    argmaxed=torch.max(out,1)[1]
+                    inp=argmaxed.unsqueeze(1).unsqueeze(0)
+                    predicted.append(argmaxed)
                 return predicted
-            return output, trg, att
+            return output, trg, attended, torch.tensor(t_tmask_trg)
         features = self.build_features(h)
 
         output = self.mlp(features)